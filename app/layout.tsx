c765a11000cc2d03efe46832068d2ae532315252
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-09-12 00:25:57 +0530

    worked on the landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,31 +27,27 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-  const session = await auth()
+  const session = await auth();
 
   return (
     <SessionProvider session={session}>
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-         <ThemeProvider
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
             <div className="flex flex-col min-h-screen">
-              <Toaster/>
-    <div className="flex-1">
-{children}
-    </div>
+              <Toaster />
+              <div className="flex-1">{children}</div>
             </div>
-        
-        </ThemeProvider>
-      </body>
-    </html>
+          </ThemeProvider>
+        </body>
+      </html>
     </SessionProvider>
   );
 }
